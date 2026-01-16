387b3156d6ffc4b4cd7580c031a04ef50b43c660
angganyobaIT <anggaradennise7@gmail.com>
2026-01-16 23:47:01 +0700

    add register and login view

---
+++ resources/views/RegisterView.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MedCare+</title>
+</head>
+<body>
+    <h1>
+        Create Account
+    </h1>
+    <form action="registerproccess" method="POST">
+        <input type="text" placeholder="Username" required >
+        <input type="email" placeholder="Email" required >
+        <input type="password" placeholder="Password" required autocomplete="new-password">
+        <input type="password" placeholder="Confirm Password" required autocomplete="new-password">
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
