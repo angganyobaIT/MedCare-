264774538dd5e821661eb1a24b10cd2e17c7ce01
angganyobaIT <anggaradennise7@gmail.com>
2026-01-25 02:09:16 +0700

    relation country -> city done

--- app/Models/city.php
+++ app/Models/city.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class city extends Model
+class City extends Model
 {
-    //
+    protected $primaryKey='city_id';
+    protected $fillable=["city_name", 'province_id'];
+
+    public function province():BelongsTo{
+        return $this->belongsTo(Province::class, 'province_id', 'province_id');
+    }
 }
--- app/Models/province.php
+++ app/Models/province.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class province extends Model
+class Province extends Model
 {
+    protected $primaryKey = 'province_id';
 
+    protected $fillable=['province_name', 'country_id'];
+
+    public function country():BelongsTo{
+        return $this->belongsTo(Country::class, 'country_id', 'country_id');
+    }
+
+    public function cities():HasMany{
+        return $this->hasMany(City::class, 'province_id', 'province_id');
+    }
 }
